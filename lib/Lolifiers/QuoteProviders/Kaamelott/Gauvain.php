b4db617195eefb20e753f749a3456835052ed9af
Claude LE BRIS <claude.lebris@gmail.com>
2015-02-10 21:57:24 +0100

    feat: Add Kaamelott quote provider Yvain and Gauvain

---
+++ lib/Lolifiers/QuoteProviders/Kaamelott/Gauvain.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Monolol\Lolifiers\QuoteProviders\Kaamelott;
+
+use Monolol\Lolifier;
+use Monolol\Lolifiers\QuoteProvider;
+
+class Gauvain implements QuoteProvider
+{
+    public function getQuotes()
+    {
+        return array(
+            "Mon oncle, mon cœur est empli de la noblesse de cette quête !",
+            "Et à un moment... Le sorcier s'est mis à nous menacer, avec ses parties génitales.",
+            "Mon oncle, n'est-ce pas vous même qui m'avez dit que rien ne remplaçait une véritable amitié et qu'en cas d'urgence c'était la seule corde à laquelle on pouvait vraiment se raccrocher ?",
+            "Noblesse bien remisée ne trouve jamais l'hiver à sa porte... Non, porte close...",
+            "Nous sommes jeunes, nous marchons à pieds… J’opterai donc pour un surnom en rapport : \"les Petits Pédestres\".",
+            "Seigneur Bohort, pouvons-nous nous retirer afin d'aller prendre notre goûter ?",
+        );
+    }
+}
---
+++ lib/Lolifiers/QuoteProviders/Kaamelott/Yvain.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Monolol\Lolifiers\QuoteProviders\Kaamelott;
+
+use Monolol\Lolifier;
+use Monolol\Lolifiers\QuoteProvider;
+
+class Yvain implements QuoteProvider
+{
+    public function getQuotes()
+    {
+        return array(
+            "Je refuse d’aller me battre pour soutenir une politique d’expansion territoriale dont je ne reconnais pas la légitimité.",
+            "Est-ce qu’on a le droit de boire du cidre ?",
+            "Je le prends pas le bouclier, ça fait trop nul !",
+            "Mon père, y dit toujours qu'on arrive jamais en prison par hasard.",
+            "Moi, je m'en fous, si on me force à y retourner, je retiens ma respiration jusqu'à ce qu'on arrête de me forcer à y retourner.",
+            "Quand il a ouvert le ventre du troll, j'ai reçu un jet d'acide qui m'a pratiquement acidifié !",
+            "Moi, je me suis fait dérober de l'alimentation tout le long du voyage ! Une véritable dérobade !",
+            "Et moi, j'ai sympathisé avec un crémier qui m'a expliqué les tenants et les aboutissants de sa profession avec beaucoup de simplicité !",
+            "J’étais tellement en colère, je leur ai lancé un de ces regards… Ils sont pas venus chercher la monnaie de leur pièce.",
+            "Ben Gauvain il est revenu, il s'était planté une écharde comme ça dans le doigt… N’empêche, si ça s’infecte, on peut en mourir, j’vous ferais dire.",
+            "J’estime ne pas avoir à subir les fantasmes carriéristes d’une entité générationnelle réactionnaire et oppressive !",
+            "La chevalerie, c'est pas là où on range les chevaux ?",
+            "Ouais bah vous encore ça va, moi, même le mot vocabulaire je suis pas sûr à 100% !",
+            "Est-ce qu'on peut s'en servir pour donner de l'élan à un pigeon ?",
+            "Je crois que je me suis coupé la gencive avec un grumeau cuit. C'est possible ou pas ?",
+            "Dans ma chambre c'est le bordel, ca pue et il y a des charençons.",
+            "Et si on envoyait quelqu’un qui se faufile dans leur camp sans se faire repérer pour observer leurs faits et gestes ? […] Ou alors, ça fait doublon avec l’espionnage ? Je me rends pas compte.",
+            "Parce que en fait dans notre langue, il y a que deux mots qui riment avec complète : c'est quiquette et biseautée. Alors ça va être chaud !",
+            "Ne vous laissez pas embobiner… Ils cherchent à vous rembobiner !",
+            "Ça pique trop de la vie, quoi : ça pique, ça lance, et derrière, comment ça re-pique trop.",
+            "Vous pouvez pas savoir comment on est trop jouasse d’être là !",
+            "Ouais, alors là où je vois que je suis vraiment à cran, c'est que quand vous avez dit malédiction, j'ai fait une série de tout petits pets comme ça : pft pft pft pft pft pft.",
+            "Mais c'est trop sur votre route quoi ! Bon y'a une colline, deux collines du coup bi-collines et après c'est carrément votre route !",
+            "Femme, cesse donc de nous esbaudir les oreilles ! Olala, il suffit !",
+            "Ah bravo femme. Ce repas fut des plus roboratif !",
+            "Femme ! Tu nous abaubis les oreilles ! Rabats un peu ta casquette !",
+        );
+    }
+}
