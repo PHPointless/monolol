9d883da7a956a554f90149ed78348ae8feac18db
Claude LE BRIS <claude.lebris@gmail.com>
2015-02-11 13:46:34 +0100

    fix: Add missing tests forquote collection

--- tests/Lolifiers/QuoteProviders/CollectionTest.php
+++ tests/Lolifiers/QuoteProviders/CollectionTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Monolol\Lolifiers\QuoteProviders;
+use Monolol\Lolifiers\QuoteProviders\Collection;
 
 class CollectionTest extends \PHPUnit_Framework_TestCase
 {
@@ -25,7 +26,15 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
         $perceval = new QuoteProviders\Kaamelott\Perceval();
         $perceval2 = new QuoteProviders\Kaamelott\Perceval();
         $karadoc = new QuoteProviders\Kaamelott\Karadoc();
+        $merlin = new QuoteProviders\Kaamelott\Merlin();
+        $kadoc = new QuoteProviders\Kaamelott\Kadoc();
 
+        $collectionA = new Collection();
+        $collectionA->add($perceval)->add($karadoc);
+
+        $collectionB = new Collection();
+        $collectionB->add($merlin)->add($kadoc);
+        
         return array(
             'Empty + Empty' => array(array(), array($emptyProvider, $emptyProvider)),
             'Same class, different instance' => array($perceval->getQuotes(), array($perceval, $perceval2)),
@@ -34,6 +43,9 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
             'Empty + Karadoc + Empty' => array($karadoc->getQuotes(), array($emptyProvider, $karadoc, $emptyProvider)),
             'Perceval + Karadoc' => array(array_merge($perceval->getQuotes(), $karadoc->getQuotes()), array($perceval, $karadoc)),
             'Perceval + Perceval + Karadoc' => array(array_merge($perceval->getQuotes(), $karadoc->getQuotes()), array($perceval, $perceval, $karadoc)),
+            'Collection + empty' => array($collectionA->getQuotes(), array($collectionA, $emptyProvider)),
+            'Collection + Kadoc' => array(array_merge($collectionA->getQuotes(), $kadoc->getQuotes()), array($collectionA, $kadoc)),
+            'Collection + Collection' => array(array_merge($collectionA->getQuotes(), $collectionB->getQuotes()), array($collectionA, $collectionB)),
         );
     }
 }
