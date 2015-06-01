3958a8178781e80191d0422f5eedddd567f479fb
Olivier Scherler <oscherler@femto-byte.com>
2015-06-01 14:42:16 +0200

    Correct grammar in the NotGiveAFuck Lolifier message.

--- lib/Lolifiers/NotGiveAFuck.php
+++ lib/Lolifiers/NotGiveAFuck.php
@@ -8,7 +8,7 @@ use Monolog\Logger;
 class NotGiveAFuck implements Lolifier
 {
     const
-        MESSAGE = "It seems that your application has encountered an issue. But as we don't give a fuck, we will not tell you what is the problem. Have a good day.";
+        MESSAGE = "It seems that your application has encountered an issue. But as we don't give a fuck, we will not tell you what the problem is. Have a good day.";
     
     public function isHandling(array $record)
     {
